dbe583c677abc2e6d2d140183e6dc1fab5a09a57
Huy Nguyen <huyndx@gmail.com>
2025-10-01 15:09:08 +0700

    Add grey 141414 theme

---
+++ install.sh
@@ -0,0 +1,4 @@
+cp -r dragonfly /usr/share/plymouth/themes/
+update-alternatives --install /usr/share/plymouth/themes/default.plymouth default.plymouth /usr/share/plymouth/themes/dragonfly/dragonfly.plymouth 80
+update-alternatives --skip-auto --config default.plymouth
+update-initramfs -u
