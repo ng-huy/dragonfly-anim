274702ab10a3d0a462497d49fc5a448142bea59d
Huy Nguyen <huyndx@gmail.com>
2025-10-01 18:56:53 +0700

    .

--- install14.sh
+++
@@ -1,4 +0,0 @@
-cp -r dragonfly14 /usr/share/plymouth/themes/
-update-alternatives --install /usr/share/plymouth/themes/default.plymouth default.plymouth /usr/share/plymouth/themes/dragonfly14/dragonfly.plymouth 80
-update-alternatives --skip-auto --config default.plymouth
-update-initramfs -u
